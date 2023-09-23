83719e0bc17658db04c0f9fa4ffa2de3ac7176df
maira <mairazamer@gmail.com>
2023-09-23 15:44:31 -0300

    Integrador Rick and Morty

--- Server/src/controllers/postFav.js
+++ Server/src/controllers/postFav.js
@@ -4,15 +4,14 @@ module.exports = async (req, res) =>{
     try {
         const {id, name, origin, status, image, species, gender} = req.body
 
-        if(!name || !origin || !status || !image || !species || !gender){
-            return res.status(401).send("Faltan datos")
+        if(name && origin && status && image && species && gender){
+            await Favorite.findOrCreate({where: { id: id, name: name, origin: origin, status: status, image: image, species: species, gender: gender }})
+            const allFavorites = await Favorite.findAll();
+            return res.status(200).json(allFavorites)
         }
-
-        await Favorite.findOrCreate({where: {id, name, origin, status, image, species, gender}});
-
-        const allFavorites = await Favorite.findAll();
-        return res.json(allFavorites)
+        return res.status(400).send("Faltan datos");
+        
     } catch (error) {
-        return res.status(500).send(error.message)
+        return res.status(500).json({error: error.message})
     }
 }
